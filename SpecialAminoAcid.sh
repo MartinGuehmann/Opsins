2b221b2cd0b9070d40872a65d4522e80f0db0ad1
Matin Gühmann <guehmann@t-online.de>
2021-07-14 13:39:01 +0100

    Add comments about the purpose of SpecialAminoAcid.sh

--- SpecialAminoAcid.sh
+++ SpecialAminoAcid.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# This is a bash config files, it is sourced so that the variables
+# defined here are available in the sourcing script.
+# The values defined here are used for displaying the amino acid of
+# interest in the visualized trees.
+# If this file does not exists then no amino acids are visualized.
+
 aaPos="296"
 seqFile="MustKeepSequences/NP_001014890_Rhodopsin_Bos_Taurus.fasta"
